4a65bc9d08145a0dd1a834a6bddb56b34baaa74d
Olexandr-Savelev <a.savelev.krm@gmail.com>
2023-01-12 13:21:29 +0200

    intermediate styles refactoring

--- pages/post/[postSlug].tsx
+++ pages/post/[postSlug].tsx
@@ -6,17 +6,12 @@ import { useRouter } from "next/router";
 import PostPageComponent from "../../components/PostPage/PostPageComponent";
 import { IPostPageProps } from "../../pageInterfaces/PostPageProps";
 
-const PostPage: NextPage<IPostPageProps> = ({ postItem, comments }) => {
+const PostPage: NextPage<IPostPageProps> = ({ postItem }) => {
   const router = useRouter();
 
   if (router.isFallback)
     return <h2 style={{ textAlign: "center" }}>LOADING...</h2>;
-  return (
-    <PostPageComponent
-      postItem={postItem}
-      comments={comments}
-    />
-  );
+  return <PostPageComponent postItem={postItem} />;
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -40,15 +35,9 @@ export const getStaticProps: GetStaticProps = async (context) => {
   );
   const postItem = await postRes.json();
 
-  const commetsRes = await fetch(
-    "https://jsonplaceholder.typicode.com/comments?_limit=10"
-  );
-  const comments = await commetsRes.json();
-
   return {
     props: {
       postItem,
-      comments,
     },
   };
 };

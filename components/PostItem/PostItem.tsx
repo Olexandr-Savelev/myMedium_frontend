dc6731af7a40d1109affe8d9f5585b3e640e2ee7
Olexandr-Savelev <a.savelev.krm@gmail.com>
2023-01-13 07:31:10 +0200

    styles refactoring

--- components/PostItem/PostItem.tsx
+++ components/PostItem/PostItem.tsx
@@ -37,7 +37,7 @@ const PostItem: FC<TopPostItemProps> = ({ place, ...postItem }) => {
         </h2>
         <p className="mt-2 text-gray-600">{postItem.body}</p>
       </div>
-      <div className="flex justify-end mt-auto">
+      <div className="flex justify-end mt-2">
         <Link href={`post/${postItem.id}`}>
           <a className="inline-block px-6 py-2.5 bg-slate-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-700 hover:shadow-lg focus:bg-slate-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
             Details
--- components/PostPage/PostPageComponent.tsx
+++ components/PostPage/PostPageComponent.tsx
@@ -1,6 +1,6 @@
+import Link from "next/link";
 import { FC, useEffect, useState } from "react";
 import { IComment, IPostPageProps } from "../../pageInterfaces/PostPageProps";
-import styles from "./PostPageComponent.module.scss";
 
 const PostPageComponent: FC<IPostPageProps> = ({ postItem }) => {
   const [comments, setCommets] = useState<IComment[]>([]);
@@ -12,34 +12,37 @@ const PostPageComponent: FC<IPostPageProps> = ({ postItem }) => {
   }, []);
 
   return (
-    <div className={styles.postpage_wrapper}>
-      <div className={styles.post_wrapper}>
-        <h3 className={styles.post_heading}>{postItem.title.toUpperCase()}</h3>
-        <p className={styles.text}>{postItem.body}</p>
+    <div className="px-2">
+      <div className="flex flex-col p-8 w-full border-[1px] border-slate-400 my-4 shadow-md">
+        <h3 className="text-center text-xl font-bold uppercase">
+          {postItem.title}
+        </h3>
+        <p>{postItem.body}</p>
+        <div>
+          <Link href={`/users/${postItem.userId}`}>
+            <a className="inline-block mt-3 px-6 py-2.5 bg-slate-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-700 hover:shadow-lg active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out">
+              User Page
+            </a>
+          </Link>
+        </div>
       </div>
-      <ul className={styles.comment_list}>
-        <span
-          className="text-gray-600 text-2xl font-semibold"
-          style={{ textAlign: "center" }}
-        >
-          Comments:
-        </span>
+      <ul className="flex flex-col">
+        <span className="text-gray-600 text-2xl font-semibold">Comments:</span>
         {comments.map(
           (comment: IComment): JSX.Element => (
-            <li key={comment.id}>
-              <div className="flex justify-center mb-3">
-                <div className="rounded-lg shadow-lg bg-white min-w-[100%]">
-                  <div className="p-4">
-                    <h5 className="text-gray-900 text-xl font-medium mb-2">
-                      Name: {comment.name}
-                    </h5>
-                    <h5 className="text-gray-800 text-xl font-medium mb-2">
-                      Email: {comment.email}
-                    </h5>
-                    <p className="text-gray-700 text-base mb-2">
-                      {comment.body}
-                    </p>
-                  </div>
+            <li
+              key={comment.id}
+              className="flex justify-center mb-3"
+            >
+              <div className="rounded-lg shadow-lg bg-white min-w-[100%]">
+                <div className="p-4">
+                  <h5 className="text-gray-900 text-xl font-medium mb-2">
+                    Name: {comment.name}
+                  </h5>
+                  <h5 className="text-gray-800 text-xl font-medium mb-2">
+                    Email: {comment.email}
+                  </h5>
+                  <p className="text-gray-700 text-base mb-2">{comment.body}</p>
                 </div>
               </div>
             </li>

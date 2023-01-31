2ae8eab611715457e757e73521010accedc30fd6
Olexandr-Savelev <a.savelev.krm@gmail.com>
2023-01-31 18:55:51 +0200

    type issue fix

---
+++ components/AboutPage/ContactList/ContactList.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import ContactItem from "./ContactItem/ContactItem";
+
+const contacts = [
+  {
+    imagePath: "/social/telegram.svg",
+    contactUrl: "https://t.me/OlexandrSavelev",
+    socialNetworkName: "Telegram",
+  },
+  {
+    imagePath: "/social/gmail.svg",
+    contactUrl: "https://a.savelev.krm@gmail.com",
+    socialNetworkName: "Gmail",
+  },
+  {
+    imagePath: "/social/facebook.svg",
+    contactUrl: "https://www.facebook.com/a.savelev.krm",
+    socialNetworkName: "Facebook",
+  },
+  {
+    imagePath: "/social/linkedin.svg",
+    contactUrl: "https://www.linkedin.com/in/olexandr-savelev-93394b243/",
+    socialNetworkName: "LinkedIn",
+  },
+  {
+    imagePath: "/social/skype.svg",
+    contactUrl: "https://join.skype.com/invite/zCnUNjYHoyZU",
+    socialNetworkName: "Skype",
+  },
+];
+
+const ContactList: FC = () => {
+  return (
+    <div className="max-w-2xl mx-auto flex gap-4 justify-center flex-wrap">
+      {contacts.map((contact) => (
+        <div key={contact.socialNetworkName}>
+          <ContactItem {...contact} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ContactList;

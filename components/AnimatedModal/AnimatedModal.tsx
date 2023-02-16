cb8f7b3184fc15d1acf35814553547bf0a7c457a
Olexandr-Savelev <a.savelev.krm@gmail.com>
2023-02-16 03:24:49 +0200

    fix z-index conflict

--- components/AnimatedModal/AnimatedModal.tsx
+++ components/AnimatedModal/AnimatedModal.tsx
@@ -44,7 +44,11 @@ const AnimatedModal: FC<IAnimatedModalProps> = ({
 
   return (
     <Portal>
-      <div className="z-20 fixed inset-0 w-screen h-screen pointer-events-none">
+      <div
+        className={`z-20 fixed inset-0 w-screen h-screen ${
+          !isOpen ? "pointer-events-none" : ""
+        }`}
+      >
         <AnimatePresence>
           {isOpen && (
             <motion.div
@@ -52,9 +56,9 @@ const AnimatedModal: FC<IAnimatedModalProps> = ({
               animate={{ opacity: 1, transition: { duration: 0.2 } }}
               exit={{ opacity: 0, transition: { duration: 0.2 } }}
             >
-              <div className="fixed inset-0 bg-black opacity-25 overflow-y-hidden h-screen"></div>
+              <div className="fixed inset-0 bg-black opacity-25 overflow-y-hidden h-screen z-30"></div>
               <div
-                className="fixed top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]"
+                className="fixed top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] z-40"
                 ref={modalRef}
               >
                 {children}
